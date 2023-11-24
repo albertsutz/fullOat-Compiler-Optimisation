33aa97fe98a893420d390ca7caa34d18e311e3d7
zunedz <daffa26@gmail.com>
2023-11-24 22:41:20 +0700

    add test case

--- code/studenttests.ml
+++ code/studenttests.ml
@@ -13,9 +13,9 @@ let provided_tests : suite =
   [
     Test
       ( "[Student provided tests] reg alloc correctness tests",
-        GradedTests.pass_all_executed_oat_file oat_regalloc_quality_tests );
+        Gradedtests.pass_all_executed_oat_file oat_regalloc_quality_tests );
     Test
       ( "[Student provided tests] reg alloc quality tests",
-        quality_oat oat_regalloc_quality_tests );
+        Gradedtests.quality_oat oat_regalloc_quality_tests );
   ]
 
