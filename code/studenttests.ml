c94dac8e51f74746c652e5c4b5adba7c06b61546
zunedz <daffa26@gmail.com>
2023-11-24 22:37:50 +0700

    add test case

--- code/studenttests.ml
+++ code/studenttests.ml
@@ -1,10 +1,21 @@
 open Assert
 
-(* These tests are provided by you -- they will be graded manually *)
+let student_reg_alloc_tests =
+  [
+    ( "trieharder.oat",
+      "",
+      "YESNO0" );
+  ]
 
-(* You should also add additional test cases here to help you   *)
-(* debug your program.                                          *)
+let oat_regalloc_quality_tests = student_reg_alloc_tests
 
-let provided_tests : suite = [
-  
-] 
+let provided_tests : suite =
+  [
+    Test
+      ( "[Student provided tests] reg alloc correctness tests",
+        GradedTests.pass_all_executed_oat_file oat_regalloc_quality_tests );
+    Test
+      ( "[Student provided tests] reg alloc quality tests",
+        quality_oat oat_regalloc_quality_tests );
+  ]
+
